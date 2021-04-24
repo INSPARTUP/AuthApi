9ed4f62b331ba66922c4873ea377feda4df391c8
zaki1003 <mz.merabet@esi-sba.dz>
2021-04-24 23:48:15 +0100

    initial commit

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+//app.use(bodyParser.json())
+app.use(express.json())
 app.use(routes)
 app.use(passport.initialize())
 require('./config/passport')(passport)
