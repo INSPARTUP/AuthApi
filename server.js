536864046cbb03e5799731da92ca6f0ce5bbb7fc
zaki1003 <mz.merabet@esi-sba.dz>
2021-04-24 23:25:56 +0100

    make it better3

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(passport.initialize())
 require('./config/passport')(passport)
 
 
-let PORT = process.env.PORT || 80
+let PORT = process.env.PORT || 27133
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
 
